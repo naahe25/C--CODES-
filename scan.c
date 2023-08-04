1ea8d95963d533eef779afa52951d6dd3a7e6d9c
naahe25 <naahebd25@gmail.com>
2023-08-04 20:22:10 +0600

    Showed The use of Scanf for Taking User Input

---
+++ scan.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    int a;
+
+    printf("Enter An Integer Number:");
+    scanf("%d",&a); //here we take input frrom user
+    printf("The Entered Number Is: %d",a);
+}
